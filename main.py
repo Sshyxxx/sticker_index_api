cdc52336284eaabe0a0f7bbd8b4a4c5e9d2af7cc
Sshyxxx <154356373+Sshyxxx@users.noreply.github.com>
2025-02-07 20:28:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
- 
-sticker_index_api = FastAPI()
- 
-@sticker_index_api.get("/ocr_get_text")
-def root():
-    return {"message": "text"}
+from fastapi import FastAPI, Query
+import requests
+from PIL import Image
+from io import BytesIO
 
-@sticker_index_api.get("/")
-def root():
-    return FileResponse("index.html")
- 
-# альтернативный вариант
-@sticker_index_api.get("/file", response_class = FileResponse)
-def root_html():
-    return "index.html"
+app = FastAPI()
 
+@app.get("/items/{item_id}")
+def process_url_image(item_id: int, image_url: str = Query(None), q: str | None = None):
+    if image_url is not None:
+        response = requests.get(image_url)
+        img = Image.open(BytesIO(response.content))
+        print(img.size)
+    
+    return {
+        "item_id": item_id,
+        "image_url": image_url,
+        "q": q
+    }
+
+#https://i1.sndcdn.com/avatars-000581659953-k4k5gk-t500x500.jpg
+#curl -Uri GET http://localhost:80/items/123?image_url=https://i1.sndcdn.com/avatars-000581659953-k4k5gk-t500x500.jpg&q=example_query
